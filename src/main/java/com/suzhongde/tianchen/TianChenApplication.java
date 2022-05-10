f272770309cfc243dbbf5c402fb7626de03d6886
suzhongde <2373544536@qq.com>
2022-05-10 16:50:04 +0800

    first commit

---
+++ src/main/java/com/suzhongde/tianchen/TianChenApplication.java
@@ -0,0 +1,13 @@
+package com.suzhongde.tianchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianChenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianChenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suzhongde/tianchen/TianChenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suzhongde.tianchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TianChenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
